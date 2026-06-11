3cf6c022d546bd651d88f615d1384c1dc263ac00
dharmendra kumar <dharmendra.kumar@justice.gov.uk>
2026-06-11 17:09:23 +0100

    Initialize alee/update-to-latest-deps

---
+++ server/@types/express/index.d.ts
@@ -0,0 +1,28 @@
+import { HmppsUser } from '../../interfaces/hmppsUser'
+
+export declare module 'express-session' {
+  // Declare that the session will potentially contain these additional fields
+  interface SessionData {
+    returnTo: string
+  }
+}
+
+export declare global {
+  namespace Express {
+    interface User {
+      username: string
+      token: string
+      authSource: string
+    }
+
+    interface Request {
+      verified?: boolean
+      id: string
+      logout(done: (err: unknown) => void): void
+    }
+
+    interface Locals {
+      user: HmppsUser
+    }
+  }
+}
